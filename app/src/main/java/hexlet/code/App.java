a5a21f049dc58c0bfe4afe30ba66fff34ddff37c
MouserRU <comics25@yandex.com>
2025-04-30 20:27:09 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,13 @@
 package hexlet.code;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-                System.out.println("Welcome to the Brain Games!");
+        String playerName;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        playerName = scanner.nextLine();
+        System.out.println("Hello, " + playerName + "!");
     }
 }
