b7eb2e6572bbf2e9b386fd21242351b04c258edf
MouserRU <comics25@yandex.com>
2025-05-01 20:14:20 +0300

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
 }
 
 tasks.test {
@@ -28,4 +29,5 @@ application {
 tasks.getByName("run", JavaExec::class) {
     standardInput = System.`in`
 }
+
 
