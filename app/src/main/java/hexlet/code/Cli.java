519a02105628b667e4e9fb06e60d5dbfed7efc8f
MouserRU <comics25@yandex.com>
2025-05-01 21:15:51 +0300

    Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,13 +3,12 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    static String greeting() {
+    static void greeting() {
         String playerName;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         playerName = scanner.nextLine();
         System.out.println("Hello, " + playerName + "!");
-        return playerName;
     }
 }
