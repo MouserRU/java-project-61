2a5851beb2a1ca438e1fa57690a2479dc71d1bff
MouserRU <comics25@yandex.com>
2025-05-01 21:00:15 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        String playerName;
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-        playerName = scanner.nextLine();
-        System.out.println("Hello, " + playerName + "!");
+        String itemNumber = scanner.nextLine();
+
+        if (itemNumber.equals("1")) {
+            Cli.greeting();
+        }
     }
 }
