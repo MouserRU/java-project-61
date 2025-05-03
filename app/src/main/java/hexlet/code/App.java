cbc28ee377a809ff06417c4f9b89bac9d893044f
MouserRU <comics25@yandex.com>
2025-05-03 19:28:45 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,6 +6,7 @@ public class App {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
+        System.out.println("3 - Calc");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
         Scanner scanner = new Scanner(System.in);
@@ -14,6 +15,7 @@ public class App {
         switch (itemNumber) {
             case "1" -> Even.greeting();
             case "2" -> Even.game();
+            case "3" -> Engine.engine("Calc");
         }
     }
 }
