aebc5232548e2031aaebeae3677f39f5631053a5
MouserRU <comics25@yandex.com>
2025-05-04 20:19:10 +0300

    add GCD game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,6 +7,7 @@ public class App {
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
         Scanner scanner = new Scanner(System.in);
@@ -16,6 +17,7 @@ public class App {
             case "1" -> Even.greeting();
             case "2" -> Even.game();
             case "3" -> Engine.engine("Calc");
+            case "4" -> Engine.engine("GCD");
         }
     }
 }
