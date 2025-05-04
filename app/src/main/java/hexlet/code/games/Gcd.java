6ca3e4b1fd153daabf462203b97aac1f6a091e24
MouserRU <comics25@yandex.com>
2025-05-04 20:38:07 +0300

    add Gcd.java

---
+++ app/src/main/java/hexlet/code/games/Gcd.java
@@ -0,0 +1,37 @@
+package hexlet.code.games;
+
+import java.security.SecureRandom;
+
+public class Gcd {
+
+    // Метод возвращает условие игры
+    public static String introductoryNote() { // Метод получает параметр содержащий имя игры
+        return "Find the greatest common divisor of given numbers.";
+    }
+
+    // Метод формирует вопрос и правильный ответ
+    public static String question(StringBuilder rightAnswer) {
+        SecureRandom rnd = new SecureRandom();
+        int m = rnd.nextInt(10) + 1; // Общий множитель
+        int number1 = (rnd.nextInt(10) + 1) * m; // Первое число
+        int number2 = (rnd.nextInt(10) + 1) * m; // Второе число
+
+        // Вычисление НОД
+        int gcd = 1;
+        int num1 = number1;
+        int num2 = number2;
+        while (num1 != 0) {
+            if (num1 >= num2) {
+                num1 %= num2;
+                gcd = num2;
+            } else {
+                int numTemp = num1;
+                num1 = num2;
+                num2 = numTemp;
+            }
+        }
+
+        rightAnswer = rightAnswer.append(gcd);
+        return number1 + " " + number2;
+    }
+}
