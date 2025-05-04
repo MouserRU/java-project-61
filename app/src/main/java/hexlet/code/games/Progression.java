01cef0a6be64e77eda301fe472e737dd333e9184
MouserRU <comics25@yandex.com>
2025-05-04 22:03:44 +0300

    add Progression.java

---
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -0,0 +1,32 @@
+package hexlet.code.games;
+
+import java.security.SecureRandom;
+
+public class Progression {
+
+    // Метод возвращает условие игры
+    public static String introductoryNote() { // Метод получает параметр содержащий имя игры
+        return "Find the greatest common divisor of given numbers.";
+    }
+
+    // Метод формирует вопрос и правильный ответ
+    public static String question(StringBuilder rightAnswer) {
+        SecureRandom rnd = new SecureRandom();
+        final int lenProgression = rnd.nextInt(10) +6; // устанавливаем длину прогрессии 6..15 номеров
+        final int step = rnd.nextInt(10) + 1; // Шаг прогрессии
+        int sequenceNumber = rnd.nextInt(lenProgression); // Порядковый номер искомого числа
+        String stringProgression = ""; // Строка хранящая прогрессию
+        int number = rnd.nextInt(10); // Задаём первое число прогрессии
+        for (int i = 0; i < lenProgression; i++) {
+            if (i != sequenceNumber) {
+                stringProgression += (number + " ");
+                number += step;
+            } else {
+                stringProgression += ".. ";
+                rightAnswer = rightAnswer.append(number);
+                number += step;
+            }
+        }
+        return stringProgression;
+    }
+}
