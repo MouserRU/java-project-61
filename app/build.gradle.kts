1e2f94b403dbea1c28134b8da89dd525a645bf16
MouserRU <comics25@yandex.com>
2025-06-15 19:17:18 +0300

    disabling dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+//    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+//    testImplementation("org.junit.jupiter:junit-jupiter")
 
 }
 
